9f73d58fe4d33c8220b4306114d556c0d0ee5ec5
albertfraxanetvin-create <albertfraxanetvin@gmail.com>
2025-11-06 16:49:19 +0100

    Init

---
+++ restaurant.java
@@ -0,0 +1,16 @@
+package Restaurant;
+
+import java.util.Scanner;
+
+public class restaurant {
+    Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("");
+        }
+
+    }
+
+
+}
